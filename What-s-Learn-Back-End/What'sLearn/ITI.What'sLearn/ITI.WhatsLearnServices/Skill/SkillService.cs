e436150259b9d417fc60b39062d34836e3a9f19b
salah <engsalahmahmoud0@gmail.com>
2020-05-28 08:16:19 +0200

    add ManageSkills Api

--- What-s-Learn-Back-End/What'sLearn/ITI.What'sLearn/ITI.WhatsLearnServices/Skill/SkillService.cs
+++ What-s-Learn-Back-End/What'sLearn/ITI.What'sLearn/ITI.WhatsLearnServices/Skill/SkillService.cs
@@ -39,7 +39,7 @@ namespace ITI.WhatsLearnServices
         {
             var query =
                 SkillRepo.GetAll();
-            query = query.Skip(pageIndex * pageSize).Take(pageSize);
+            query = query.OrderByDescending(i=> i.ID).Skip(pageIndex * pageSize).Take(pageSize);
             return query.ToList().Select(i => i.ToViewModel());
         }
         public IEnumerable<SkillViewModel> Get(Expression<Func<Skill, bool>> filter)
